121a788cec6d6f47b3d125a063a1196b68ca618c
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2021-11-08 13:47:27 +0200

    add slider tab

--- resources/views/dashboard/layouts/leftSidebar.blade.php
+++ resources/views/dashboard/layouts/leftSidebar.blade.php
@@ -21,6 +21,13 @@
                     <li class="has-submenu">
                         <a href="{{ route('dashboard') }}"><i class="fa fa-home"></i>@lang('dashboard.home')</a>
                     </li>
+                    <li id="itemSlider" class="has-submenu">
+                        <a href="#"><i class="fa fa-picture-o"></i>@lang('dashboard.sliders')</a>
+                        <ul class="submenu">
+                            <li><a href="">@lang('dashboard.viewAll')</a></li>
+                            <li><a href="">@lang('dashboard.addImage')</a></li>
+                        </ul>
+                    </li>
                     <li class="has-submenu">
                         <a href="logout" onclick="logout(event)"><i class="md md-swap-vert"></i>@lang('dashboard.logout')</a>
                     </li>
